785971b3009f07ae3042c6633a739a1ef31dc84c
ArslanDev666 <arslan.mustafin.dev@gmail.com>
2022-12-15 23:33:26 +0300

    feat: add cart store

---
+++ store/cart.js
@@ -0,0 +1,41 @@
+export const state = () => ({
+  items: [],
+})
+
+export const getters = {
+  getItems(state) {
+    return state.items
+  },
+
+  isProductInCart(state, id) {
+    return state.items.some((item) => item.id === id)
+  },
+}
+
+export const mutations = {
+  add(state, product) {
+    state.items.push(product)
+  },
+
+  remove(state, id) {
+    state.items = state.items.filter((item) => item.id !== id)
+  },
+
+  clear(state) {
+    state.items = []
+  },
+
+  incrementProduct(state, id) {
+    state.items = state.items.map((item) =>
+      item.id === id ? { ...item, count: item.count + 1 } : item
+    )
+  },
+
+  decrementProduct(state, id) {
+    state.items = state.items.map((item) =>
+      item.id === id ? { ...item, count: item.count - 1 } : item
+    )
+  },
+
+  
+}
